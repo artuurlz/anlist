abbe9cdaa7737c47b0de48fe9691558a6152bd75
artuurlz <arturcontatop@gmail.com>
2022-11-29 11:53:36 -0300

    config menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 // ANIMAÇÃO DE MOSTRAR O MENU AO CLICAR
 
+const navMenu = document.getElementById('nav-menu');
+const navClose = document.getElementById('nav-close');
+const navToggle = document.getElementById('nav-toggle');
+
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('nav-show');
+    });
+}
+
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('nav-show');
+    });
+}
+
 // ANIMAÇÃO SCROLLREVEAL
